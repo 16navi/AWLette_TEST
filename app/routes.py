f074d5d28b99b9de7a12404457e4d7988789607f
Lei Ivan Juarez <24514@burnside.school.nz>
2024-05-03 17:22:31 +1200

    added the about, all_words, and word pages. accomodated for these changes additions

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,20 @@ import app.models as models
 @app.route('/')
 def homepage():
     return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/all_words')
+def all_words():
+    return render_template("all_words.html")
+
+
+@app.route('/all_words/word')
+def word():
+    return render_template("word.html")
+
+
